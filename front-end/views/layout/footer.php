06407cb461dbde75ad196a3239183bd025b38d6f
Tolga <tolgaguler22@gmail.com>
2020-06-13 14:42:37 +0200

    header and footer built
    I made a layout file where de header and footer are. These get called on every page.

---
+++ front-end/views/layout/footer.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+        <footer class="card-footer bg-dark fixed-bottom w-100 m-auto" id="footer">
+            <p class="text-center text-white">Copyright 2020® Tolga Guler </p>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </footer>
+    </body>
+</html>
---
+++ front-end/views/layout/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="description" content="Blog">
+        <meta name="keywords" content="blog">
+        <meta name="author" content="Tolga Guler">
+        <title>Blog</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+        <header class="w-100">
+            <ul class="nav text-white bg-dark p-2 w-100">
+                <li class="nav-item mr-3">
+                    <a class="nav-link text-white" href="../views"><h4>Blog</h4></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="../views">Posts</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Make post</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">login</a>
+                </li>
+            </ul>
+        </header>
+    </body>
+</html>
